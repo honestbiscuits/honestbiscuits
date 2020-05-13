b7b43edc4e7100de9b639dda115ea688b9c42f5e
Todd Emerson <todd@toddemerson.com>
2020-05-13 14:33:28 -0700

    messaging update

--- src/components/messaging.js
+++ src/components/messaging.js
@@ -6,7 +6,7 @@ import biscuitMug from '../images/chicken-biscuit-mug.jpg'
 
 const Messaging = () => (
   <div className="container">
-    <h1>Open for <a href={config.userLinks[4].url}>Takeout</a> and <a href={config.userLinks[6].url}>Delivery</a>!</h1>
+    <h1>Open for <a href={config.userLinks[4].url} target="_blank">Takeout</a> and <a href={config.userLinks[6].url} target="_blank">Delivery</a>!</h1>
     <div className="d-md-inline-flex row align-items-center">
       <div className="col text-center mx-auto">
         <img className="img-fluid" src={kickAss} alt="Kick Ass Biscuits" />
